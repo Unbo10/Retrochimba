ffa81f68e90393421c4b22f53c4541ab758d302c
Inboard10 <santirochap@gmail.com>
2023-11-05 23:22:56 -0500

    V2

--- f1.py
+++ f1.py
@@ -9,32 +9,43 @@ clock = pygame.time.Clock()
 Minfont = pygame.font.Font('Minecraft.ttf',40)
 
 # Default sizes for images to scale them
-def_size_S = (225, 500)
-def_size_S_L= (245, 500)
-def_size_S_R = (246, 500)
+size_S = (225, 500)
+size_S_L= (245, 500)
+size_S_R = (246, 500)
 
 #BACKGROUND
-S_base = pygame.image.load("images/i-S.png").convert_alpha()
-S_base = pygame.transform.scale(S_base, def_size_S)
-R_S_base = S_base.get_rect(center = (300, 250))
+
+#Track
+S_start = pygame.image.load("images/i-S.png").convert_alpha()
+S_start = pygame.transform.scale(S_start, size_S)
+R_S_start = S_start.get_rect(center = (300, 250))
+S_grid = pygame.image.load("images/i-S-grid.png").convert_alpha()
+S_grid = pygame.transform.scale(S_grid, size_S)
+R_S_grid = S_grid.get_rect(midbottom = (R_S_start.midtop))
+S_finish_line = pygame.image.load("images/i-S-finish-line.png").convert_alpha()
+S_finish_line = pygame.transform.scale(S_finish_line, size_S)
+R_S_finish_line = S_finish_line.get_rect(midbottom = (R_S_grid.midtop))
+S_end = pygame.image.load("images/i-S.png").convert_alpha()
+S_end = pygame.transform.scale(S_end, size_S)
+R_S_end = S_end.get_rect(midbottom = (R_S_finish_line.midtop))
 
 S_barriers_L = pygame.image.load("images/i-S-Lbarrier.png").convert_alpha()
-S_barriers_L = pygame.transform.scale(S_barriers_L, def_size_S_L)
+S_barriers_L = pygame.transform.scale(S_barriers_L, size_S_L)
 R_S_barriers_L1 = S_barriers_L.get_rect(midright = (188, 250)) # Problem: images are not loading and they are "waving". Look at the tutorial because that did not happen there
 R_S_barriers_L2 = S_barriers_L.get_rect(midright = (188, -250))
 R_S_barriers_L3 = S_barriers_L.get_rect(midright = (188, -750))
 R_S_barriers_L4 = S_barriers_L.get_rect(midright = (188, -1250))
 
 S_pits = pygame.image.load("images/i-S-pits-NOlogos.png").convert_alpha()
-S_pits = pygame.transform.scale(S_pits, def_size_S_R)
+S_pits = pygame.transform.scale(S_pits, size_S_R)
 R_S_pits1 = S_pits.get_rect(midleft = (413, 250))
 S_pits_logos = pygame.image.load("images/i-S-pits-logos.png").convert_alpha()
-S_pits_logos = pygame.transform.scale(S_pits_logos, def_size_S_R)
+S_pits_logos = pygame.transform.scale(S_pits_logos, size_S_R)
 R_S_pits_logos = S_pits_logos.get_rect(midleft = (413, -250))
 R_S_pits2 = S_pits.get_rect(midleft = (413, -750))
-S_pits_salida = pygame.image.load("images/i-S-pits-NOlogos.png").convert_alpha() #Cambiar por salida de pits
-S_pits_salida = pygame.transform.scale(S_pits_salida, def_size_S_R)
-R_S_pits_salida = S_pits.get_rect(midleft = (413, -1250))
+S_pits_exit = pygame.image.load("images/i-S-pits-exit.png").convert_alpha() #Cambiar por salida de pits
+S_pits_exit = pygame.transform.scale(S_pits_exit, size_S_R)
+R_S_pits_exit = S_pits.get_rect(midleft = (413, -1250))
 
 #CAR AND OTHERS
 car = pygame.image.load("images/f1-mclaren.png").convert_alpha()
@@ -44,7 +55,7 @@ R_car = car.get_rect(midtop = (300, 385))
 Time = Minfont.render("1:25.000", False, "Black") #Boolean for capital letters
 
 #Variables
-car_pos_y_increment = 0
+car_pos_y_increment = float(0)
 car_pos_x_increment = 0
 game_active = True
 
@@ -60,16 +71,17 @@ while True:
         #         car_pos_y_increment -= 0.25
         # if (event.type != pygame.KEYDOWN) and (car_pos_y_increment > 0):
         #         car_pos_y_increment -= 0.25
+    screen.fill((0, 0, 0))
 
     if game_active == True:
         keys = pygame.key.get_pressed() #Get all the keys getting pressed in every frame
         car_pos_x_increment = 0
         if keys[pygame.K_UP]:
-            car_pos_y_increment += 0.05 #Replace for function of acceleration
+            car_pos_y_increment += 0.10 #Replace for function of acceleration
         elif (car_pos_y_increment > 0):
-            car_pos_y_increment -= 0.10
+            car_pos_y_increment -= 0.05
             if keys[pygame.K_LEFT]:
-                    car_pos_x_increment -= 1
+                car_pos_x_increment -= 1
             elif keys[pygame.K_RIGHT]:
                 car_pos_x_increment += 1 
             else:
@@ -78,47 +90,56 @@ while True:
         if R_car.y < 100:
             game_active = False
 
+        print(R_S_start.top - R_S_grid.bottom, car_pos_y_increment)
+
         #SHOW BACKGROUND
-        R_S_base.x -= car_pos_x_increment
-        R_S_barriers_L1.x -= car_pos_x_increment
-        R_S_barriers_L2.x -= car_pos_x_increment
-        R_S_barriers_L3.x -= car_pos_x_increment
-        R_S_barriers_L4.x -= car_pos_x_increment
-        R_S_pits1.x -= car_pos_x_increment
-        R_S_pits_logos.x -= car_pos_x_increment
-        R_S_pits2.x -= car_pos_x_increment
-        R_S_pits_salida.x -= car_pos_x_increment
+        R_S_start.x -= int(car_pos_x_increment)
+        R_S_barriers_L1.x -= int(car_pos_x_increment)
+        R_S_pits1.x -= int(car_pos_x_increment)
+        R_S_grid.x -= int(car_pos_x_increment)
+        R_S_barriers_L2.x -= int(car_pos_x_increment)
+        R_S_pits_logos.x -= int(car_pos_x_increment)
+        R_S_finish_line.x -= int(car_pos_x_increment)
+        R_S_barriers_L3.x -= int(car_pos_x_increment)
+        R_S_pits2.x -= int(car_pos_x_increment)
+        R_S_end.x -= int(car_pos_x_increment)
+        R_S_barriers_L4.x -= int(car_pos_x_increment)
+        R_S_pits_exit.x -= int(car_pos_x_increment)
+        
+        R_S_start.y += int(car_pos_y_increment)
+        R_S_barriers_L1.y += int(car_pos_y_increment)
+        R_S_pits1.y += int(car_pos_y_increment)
+        R_S_grid.y += int(car_pos_y_increment)
+        R_S_barriers_L2.y += int(car_pos_y_increment)
+        R_S_pits_logos.y += int(car_pos_y_increment)
+        R_S_finish_line.y += int(car_pos_y_increment)
+        R_S_barriers_L3.y += int(car_pos_y_increment)
+        R_S_pits2.y += int(car_pos_y_increment)
+        R_S_end.y += int(car_pos_y_increment)
+        R_S_barriers_L4.y += int(car_pos_y_increment)
+        R_S_pits_exit.y += int(car_pos_y_increment)
 
-        R_S_base.y += car_pos_y_increment
-        R_S_barriers_L1.y += car_pos_y_increment
-        R_S_barriers_L2.y += car_pos_y_increment
-        R_S_barriers_L3.y += car_pos_y_increment
-        R_S_barriers_L4.y += car_pos_y_increment
-        R_S_pits1.y += car_pos_y_increment
-        R_S_pits_logos.y += car_pos_y_increment
-        R_S_pits2.y += car_pos_y_increment
-        R_S_pits_salida.y += car_pos_y_increment
-
-        screen.blit(S_base, R_S_base)
-        screen.blit(S_barriers_L, R_S_barriers_L1)
-        screen.blit(S_barriers_L, R_S_barriers_L2)
-        screen.blit(S_barriers_L, R_S_barriers_L3)
-        screen.blit(S_barriers_L, R_S_barriers_L4)
+        screen.blit(S_start, R_S_start)
+        screen.blit(S_barriers_L, R_S_barriers_L1)        
         screen.blit(S_pits, R_S_pits1)
+        screen.blit(S_grid, R_S_grid)
+        screen.blit(S_barriers_L, R_S_barriers_L2)
         screen.blit(S_pits_logos, R_S_pits_logos)
+        screen.blit(S_finish_line, R_S_finish_line)
+        screen.blit(S_barriers_L, R_S_barriers_L3)
         screen.blit(S_pits, R_S_pits2)
-        screen.blit(S_pits_salida, R_S_pits_salida)
+        screen.blit(S_end, R_S_end)
+        screen.blit(S_barriers_L, R_S_barriers_L4)
+        screen.blit(S_pits_exit, R_S_pits_exit)
 
         #SHOW CAR
         screen.blit(car, R_car)
 
-        # pygame.draw.arc(track, (200, 200, 200), track_curve_rect, m.pi * 0.5, m.pi, 50)
-
         screen.blit(Time, (225,15))
-    
+        pygame.display.update()
+        clock.tick(30)
     else:
-        S_base.set_alpha(100)
-        print(S_base.get_alpha())
+        S_finish_line.set_alpha(100)
+        print(S_finish_line.get_alpha())
 
-    pygame.display.update()
-    clock.tick(30)
+    
